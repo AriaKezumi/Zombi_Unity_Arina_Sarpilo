0f8c7b16513acc427978e5ba37a0095ad9e1a535
Lawrentique <ari4games0@gmail.com>
2026-02-09 15:49:24 +0200

    Assets, zombie, code

---
+++ Zombi_Uni/Assets/Zombies/Assets/Script/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] zombies;
+    public GameObject selectedZombie;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        selectedZombie = zombies[2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
